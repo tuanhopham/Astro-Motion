32146b7bd5d5f802a280c1fcb08a440d8fd87362
phạm tuấn <minhtuanpham2111@gmail.com>
2022-09-26 16:20:02 +0700

    responsive

--- js.js
+++ js.js
@@ -6,19 +6,39 @@ $(document).ready(function(){
         dots: true,
         infinite: true,
         arrows: false,
-       
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              
+              }
+            }
+    
+          ]
     });
   });
 
 const $1 = document.querySelector.bind(document);
 const $$2 = document.querySelectorAll.bind(document);
 const viewGalleryBtn = $1('.js-submit-gallery a')
+const nav = $1('.header .nav-list')
 const navList = $$2('.nav-list li')
 const contents = $$2('.content>div')
+const menuBox = $1('.menu-box')
     navList.forEach(navItem => {
-       
+       //khi bấm vào navItem
         navItem.onclick = function(){
             removeContent();
+            nav.classList.toggle('show') 
         //thêm active và xóa active trên navlist
             $1('a.ac').classList.remove('ac')
             $1('div.ac').classList.remove('ac')
@@ -47,3 +67,16 @@ window.addEventListener('load',vanish)
 function vanish(){
     preloader.classList.add('disppear')
 }
+
+//--------------responsive--------------
+//khi ấn vào menuBox thì hiện menu
+menuBox.onclick = function(e){
+    nav.classList.toggle('show')    
+    e.stopPropagation();
+}
+//khi ấn ra khỏi menu thì menu tự đóng
+$(window).click(function() {
+    nav.classList.remove('show') 
+  });
+  
+  
